562b3fe256d800c3b339a870620c368513731b3d
fsmirt <fs.dev@mirt.fr>
2016-04-23 16:02:38 +0200

    --

---
+++ test/src/view/Identification.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Frank on 23/04/2016.
+ */
+public class Identification {
+
+    private JPanel Connection;
+    private JLabel serverNameLabel;
+    private JTextField serverNameJT;
+    private JLabel serverPortLabel;
+    private JTextField serverPortJT;
+    private JTextField pseudoJT;
+    private JLabel nickNameLabel;
+
+    public static void main(String[] args) {
+
+
+    }
+}
