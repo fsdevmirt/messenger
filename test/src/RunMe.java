e1c973980f3e772403868699205b4f514ada783b
fsmirt <fs.dev@mirt.fr>
2016-05-12 22:39:43 +0200

    --

---
+++ test/src/RunMe.java
@@ -0,0 +1,13 @@
+import view.Connection;
+
+/**
+ * Created by Frank on 11/05/2016.
+ */
+public class RunMe {
+
+    public static void main(String[] args) {
+
+        Connection window = new Connection();
+
+    }
+}
